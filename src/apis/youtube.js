bfd2e8b7c5632e60c55072ac97c116c1d65d247b
Salome Ranson <salomeranson@gmail.com>
2021-09-09 18:48:07 -0400

    application complete. Working as expectred locally

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -7,7 +7,7 @@ export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
     params: {
         part: 'snippet',
-        maxResults: 5,
+        maxResults: 8,
         key: KEY
     }
 });
