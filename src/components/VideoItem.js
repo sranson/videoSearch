6352ee7bb3c85d3b6f6ebd76cd788d104fb38b6f
Salome Ranson <salomeranson@gmail.com>
2021-09-09 18:29:38 -0400

    rendering and playing selected video

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -2,9 +2,9 @@ import React from 'react';
 import "./VideoItem.css";
 
 
-const VideoItem = ({ video }) => {
+const VideoItem = ({ video, onVideoSelect }) => {
     return(
-        <div className="video-item item">
+        <div className="video-item item" onClick={() => onVideoSelect(video)}>
             <img alt={video.snippet.title} className="ui image" src={video.snippet.thumbnails.medium.url} />
             <div className="content">
                 <div className="header">{video.snippet.title}</div>
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import VideoItem from './VideoItem';
 
-const VideoList = ({ videos }) => {
+const VideoList = ({ videos, onVideoSelect }) => {
 
-    const renderedList = videos.map((video) => {
-        return <VideoItem video={video} key={ video.snippet.title }/>
-    })
+    const renderedList = videos.map(video => {
+        return <VideoItem video={video} onVideoSelect={onVideoSelect} key={ video.snippet.title }/>
+    });
 
     return(
         <div className="ui relaxed divided list">
