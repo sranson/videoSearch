ff7f838740977246955e9da96be1a1447668c916
Salome Ranson <salomeranson@gmail.com>
2021-09-09 19:04:02 -0400

    updated README

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -7,7 +7,7 @@ export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
     params: {
         part: 'snippet',
-        maxResults: 8,
+        maxResults: 5,
         key: KEY
     }
 });
