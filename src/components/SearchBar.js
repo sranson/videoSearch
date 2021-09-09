c87344df59134050cbd3def2a6a78ae3275ebdc8
Salome Ranson <salomeranson@gmail.com>
2021-09-09 17:21:43 -0400

    making successful api call to youtube api

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -10,7 +10,8 @@ class SearchBar extends React.Component {
 
   onFormSubmit = (event) => {
     event.preventDefault();
-    // TODO: Make sure we call callback from parent component
+    this.props.onFormSubmit(this.state.term);
+    this.setState({ term: ""})
   }
 
   render() {
