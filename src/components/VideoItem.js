4fedda2e40fc4b4e50b7e91633cef92dd46feebe
Salome Ranson <salomeranson@gmail.com>
2021-09-09 17:53:59 -0400

    added styling to youtube results

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import "./VideoItem.css";
 
 
 const VideoItem = ({ video }) => {
     return(
-        <div>
-            <img src={video.snippet.thumbnails.medium.url} />
-            {video.snippet.title}
+        <div className="video-item item">
+            <img alt={video.snippet.title} className="ui image" src={video.snippet.thumbnails.medium.url} />
+            <div className="content">
+                <div className="header">{video.snippet.title}</div>
+            </div>
         </div>
     )
 };
